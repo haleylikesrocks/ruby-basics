e3e545e3fced126910b34646aec664675b28e517
Haley Rairez <ramirezhaley@gmail.com>
2020-12-18 15:20:52 -0500

    The basic excersices from intro to programming book from Launch School

---
+++ The_Basics/exercises.rb
@@ -0,0 +1,40 @@
+# Excercise one
+first = "Haley "
+last = "Ramirez"
+puts first + last
+
+# Exercise two
+number = 1234
+thousands = number / 1000 % 10
+hundreds = number / 100 % 10
+tens = number / 10 % 10
+ones = number % 10
+puts thousands
+puts hundreds
+puts tens
+puts ones
+
+# Exercise three 
+movie_dates = {movie1: 1980, movie2: 1990}
+puts movie_dates[:movie1]
+puts movie_dates[:movie2]
+
+# Exercise four
+movie_array = [1980, 1990]
+puts movie_array[0]
+puts movie_array[1]
+
+# Exercise five
+puts 5 * 4 * 3 * 2
+puts 6 * 5 * 4 * 3 * 2
+puts 7 * 6 * 5 * 4 * 3 * 2
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2
+
+# Exercise six
+puts 4.3 ** 2
+puts 5.4 ** 2
+puts 1.2 ** 2
+
+# Exercise 7
+
+# you should replace ) with } in line 2 of the irb editor
